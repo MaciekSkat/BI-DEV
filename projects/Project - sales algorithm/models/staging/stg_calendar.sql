8626571a2d544d1f0db1ee3799e9577142da1a8a
MaciekSkat <skatman63@gmail.com>
2026-09-19 21:42:55 +0200

    add file sql and yml of stg_calendar

---
+++ projects/Project - sales algorithm/models/staging/stg_calendar.sql
@@ -0,0 +1,16 @@
+select
+    [date]      as calendar_date,
+    d           as day_id,
+    wm_yr_wk    as week_id,
+    weekday     as day_name,
+    wday        as day_of_week_num,
+    month       as month_num,
+    [year]      as year_num,
+    event_name_1,
+    event_type_1,
+    event_name_2,
+    event_type_2,
+    snap_CA     as is_snap_ca,
+    snap_TX     as is_snap_tx,
+    snap_WI     as is_snap_wi
+from {{ ref('calendar') }}
